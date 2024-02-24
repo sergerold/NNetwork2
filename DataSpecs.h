697be8f3a49242d2e6b0e986bfa277fd16afef9d
tom <hepplewhite@gmail.com>
2024-02-24 21:59:59 +0000

    adding comments

--- DataSpecs.h
+++ DataSpecs.h
@@ -5,8 +5,8 @@
 #ifndef NNETWORK2_DATASPECS_H
 #define NNETWORK2_DATASPECS_H
 
-constexpr Eigen::Index INPUT_SZ = 784;
+constexpr Eigen::Index INPUT_SZ = 784; // number of inputs
 #define CLASSES "0", "1", "2" , "3", "4", "5", "6", "7", "8", "9"
-#define NUM_TYPE float
+#define NUM_TYPE float // float or double
 
 #endif //NNETWORK2_DATASPECS_H
